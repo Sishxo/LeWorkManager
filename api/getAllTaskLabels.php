9af5c3e81f267868ee76ead4ca644dbe525e2701
sishxo <10180657@mail.ecust.edu.cn>
2022-07-30 22:30:58 +0800

    task label history hint done.

---
+++ api/getAllTaskLabels.php
@@ -0,0 +1,51 @@
+<?php
+    // settings
+    header('Access-Control-Allow-Origin: *');
+	ini_set("display_errors","Off");
+
+    // $passwd = $_POST['passwd'];
+    // $res = [$email, $passwd];
+
+    // 数据库
+    include("mysqlConnect.php");
+	if (!$conn)
+	{
+		$res['code'] = 0;
+		$res['message'] = "连接错误";
+	}
+	else
+	{
+		$sql = "SELECT DISTINCT label FROM `platform_projectLabel` order by id desc limit 10";
+		// echo $sql;
+		
+		$result = mysqli_query($conn, $sql);
+		
+		if ($result)
+		{
+			$resMessage = array();
+			while ($row = mysqli_fetch_assoc($result))
+			{
+				$res['code'] = 200;
+				$resMessage[] = $row;
+			}
+
+			if (count($resMessage))
+			{
+				$res['code'] = 200;
+				$res['message'] = $resMessage;
+			}
+			else
+			{
+				$res['code'] = 20;
+				$res['message'] = "未查询到数据";
+			}
+		}
+		else
+		{
+			$res['code'] = 10;
+			$res['message'] = "查询失败";
+		}
+	}
+
+    echo json_encode($res, JSON_FORCE_OBJECT);
+?>
